59fb8e4e2c850e7b187261f64a507ea7bf5797d4
pragyann007 <pragyanthapaliya027@gmail.com>
2026-06-25 14:27:12 +0545

    first commit

---
+++ indes.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+const port = 3000;
+
+app.get("/",(req,res)=>{
+
+    res.send("hey dude.")
+})
+
+app.listen(port,()=>{
+    console.log(`server runing at ${port}`)
+})
